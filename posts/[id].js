17134642c2fa3c3acb0278f7a3e032455adcefb1
heroncarli <eng.heroncarli@gmail.com>
2024-08-29 10:20:29 -0300

    mudanças e melhorias feitas no consumo de API nextjs+supabase

--- posts/[id].js
+++ posts/[id].js
@@ -1,17 +1,17 @@
-import { getGlobalData } from '../../utils/global-data';
+import { getGlobalData } from '../utils/global-data';
 import {
   getPostBySlug,
-} from '../../utils/mdx-utils';
+} from '../utils/mdx-utils';
 
 import { MDXRemote } from 'next-mdx-remote';
 import Head from 'next/head';
 import Link from 'next/link';
-import ArrowIcon from '../../components/ArrowIcon';
-import CustomLink from '../../components/CustomLink';
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
-import Layout, { GradientBackground } from '../../components/Layout';
-import SEO from '../../components/SEO';
+import ArrowIcon from '../components/ArrowIcon';
+import CustomLink from '../components/CustomLink';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Layout, { GradientBackground } from '../components/Layout';
+import SEO from '../components/SEO';
 
 
 const components = {
